3c5e84737c87a4c81a8b6c876d93b6391262e004
Alan O'Donnell <alan.m.odonnell@gmail.com>
2012-09-12 11:27:22 -0400

    Split and parse incoming TCP msgs by IRC protocol

--- src/cerlicue_tcp_handler.erl
+++ src/cerlicue_tcp_handler.erl
@@ -1,7 +1,7 @@
 -module(cerlicue_tcp_handler).
 -behaviour(gen_server).
 
--record(state, {sock}).
+-record(state, {sock, incomplete_msg=""}).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
@@ -41,6 +41,19 @@ handle_info(timeout, LSock) ->
     {ok, Sock} = gen_tcp:accept(LSock),
     cerlicue_tcp_handler_sup:start_child(),
     {noreply, #state{sock=Sock}};
+handle_info({tcp, _Sock, Data}, State=#state{incomplete_msg=IncompleteMsg}) ->
+    case split_by_crlf(Data) of
+        {[FirstMsg|RestMsgs], Partial} ->
+            CompletedMsg = IncompleteMsg ++ FirstMsg,
+            Parsings = lists:map(fun cerlicue_parser:parse/1, [CompletedMsg|RestMsgs]),
+            lists:foreach(make_msg_handler(State), Parsings),
+            NewState = State#state{incomplete_msg=Partial};
+        {[], Partial} ->
+            NewState = State#state{incomplete_msg=IncompleteMsg ++ Partial}
+    end,
+    {noreply, NewState};
+handle_info({tcp_closed, _Sock}, State) ->
+    {stop, normal, State}.
 
 terminate(_Reason, _State) ->
     ok.
@@ -49,5 +62,28 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 %% ------------------------------------------------------------------
+%% Internal IRC callbacks
+%% ------------------------------------------------------------------
+
+make_msg_handler(#state{sock=Sock}) ->
+    fun (Parsing) ->
+            Response = io_lib:format("~p~c~c", [Parsing, $\r, $\n]),
+            gen_tcp:send(Sock, Response)
+    end.
+
+%% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+
+% CrlfSeparatedMsgs -> {[Msgs], PartialMsg}
+split_by_crlf(CrlfSeparatedMsgs) ->
+    split_by_crlf(CrlfSeparatedMsgs, "", []).
+
+split_by_crlf("", ReversedPartial, Accumulated) ->
+    Partial = lists:reverse(ReversedPartial),
+    {lists:reverse(Accumulated), Partial};
+split_by_crlf("\r\n" ++ Msg, ReversedPartial, Accumulated) ->
+    Complete = lists:reverse(ReversedPartial),
+    split_by_crlf(Msg, "", [Complete|Accumulated]);
+split_by_crlf([C|Rest], ReversedPartial, Accumulated) ->
+    split_by_crlf(Rest, [C|ReversedPartial], Accumulated).
