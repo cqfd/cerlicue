32e85f809f5ee8ba8a0f7cda574e77623e25b915
Alan O'Donnell <alan.m.odonnell@gmail.com>
2012-10-15 14:09:46 -0400

    Bunch of misc progress on the router

--- src/cerlicue_router.erl
+++ src/cerlicue_router.erl
@@ -1,12 +1,13 @@
 -module(cerlicue_router).
 -behaviour(gen_server).
+
 -define(SERVER, ?MODULE).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/0, register/2, unregister/1, join/2, privmsg/2, names/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -22,19 +23,75 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+register(Nick, Pid) ->
+    gen_server:call(?SERVER, {register, Nick, Pid}).
+
+join(Pid, Channel) ->
+    gen_server:call(?SERVER, {join, Pid, Channel}).
+
+privmsg(Nick, Msg) ->
+    gen_server:call(?SERVER, {privmsg, Nick, Msg}).
+
+% Unregister/quit. Removes the nick from the db. Quit may be a nicer
+% name, matches the IRC protocol.
+unregister(Nick) ->
+    gen_server:cast(?SERVER, {unregister, Nick}).
+
+names(ChannelNick) ->
+    gen_server:call(?SERVER, {names, ChannelNick}).
+
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init([]) ->
+    {ok, dict:new()}.
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+% Callback for registering a nick.
+handle_call({register, Nick, Pid}, _From, Map) ->
+    case dict:find(Nick, Map) of
+        {ok, Pid} ->
+            {reply, ok, Map};
+        {ok, _DifferentPid} ->
+            {reply, {error, nick_taken}, Map};
+        error ->
+            NewMap = dict:store(Nick, Pid, Map),
+            {reply, ok, NewMap}
+    end;
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+handle_call({privmsg, Nick, Msg}, _From, Map) ->
+    case dict:find(Nick, Map) of
+        {ok, Pid} ->
+            Pid ! Msg,
+            {reply, ok, Map};
+        error ->
+            {reply, {error, nick_not_found}, Map}
+    end;
 
+handle_call({join, Pid, ChannelNick}, _From, Map) ->
+    case dict:find(ChannelNick, Map) of
+        {ok, ChannelPid} ->
+            cerlicue_channel:join(ChannelPid, Pid),
+            {reply, ok, Map};
+        error ->
+            {ok, ChannelPid} = cerlicue_channel_sup:start_child(ChannelNick),
+            NewMap = dict:store(ChannelNick, ChannelPid, Map),
+            cerlicue_channel:join(ChannelPid, Pid),
+            {reply, ok, NewMap}
+    end;
+
+handle_call({names, ChannelNick}, _From, Map) ->
+    case dict:find(ChannelNick, Map) of
+        {ok, ChannelPid} ->
+            {reply, cerlicue_channel:names(ChannelPid), Map};
+        error ->
+            {reply, error, Map}
+    end.
+
+% Callback for unregistering a nick from the router.
+handle_cast({unregister, Nick}, Map) ->
+    NewMap = dict:erase(Nick, Map),
+    {noreply, NewMap}.
 handle_info(_Info, State) ->
     {noreply, State}.
 
