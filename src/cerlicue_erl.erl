0680325556c19e8c0077aa49aedc0c334a49f1e8
Alan O'Donnell <alan.m.odonnell@gmail.com>
2012-10-31 10:47:21 -0400

    Isomorphic gen_fsmification of cerlicue_erl
    Next step: add more states to the state machine.

--- src/cerlicue_erl.erl
+++ src/cerlicue_erl.erl
@@ -1,5 +1,5 @@
 -module(cerlicue_erl).
--behaviour(gen_server).
+-behaviour(gen_fsm).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
@@ -15,84 +15,89 @@
          quit/1]).
 
 %% ------------------------------------------------------------------
-%% gen_server Function Exports
+%% gen_fsm Function Exports
 %% ------------------------------------------------------------------
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([init/1, handle_event/3, handle_sync_event/4, handle_info/3,
+         terminate/3, code_change/4]).
+
+-export([connected/2,
+         connected/3]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
 start_link() ->
-    gen_server:start_link(?MODULE, [], []).
+    gen_fsm:start_link(?MODULE, [], []).
 nick(Pid, Nick) ->
-    gen_server:call(Pid, {nick, Nick}).
+    gen_fsm:sync_send_event(Pid, {nick, Nick}).
 privmsg(Pid, Name, Msg) ->
-    gen_server:call(Pid, {privmsg, Name, Msg}).
+    gen_fsm:sync_send_event(Pid, {privmsg, Name, Msg}).
 join(Pid, Channel) ->
-    gen_server:call(Pid, {join, Channel}).
+    gen_fsm:sync_send_event(Pid, {join, Channel}).
 topic(Pid, Channel) ->
-    gen_server:call(Pid, {topic, Channel}).
+    gen_fsm:sync_send_event(Pid, {topic, Channel}).
 names(Pid, Channel) ->
-    gen_server:call(Pid, {names, Channel}).
+    gen_fsm:sync_send_event(Pid, {names, Channel}).
 part(Pid, Channel, Msg) ->
-    gen_server:call(Pid, {part, Channel, Msg}).
+    gen_fsm:sync_send_event(Pid, {part, Channel, Msg}).
 quit(Pid) ->
-    gen_server:cast(Pid, quit).
+    gen_fsm:send_event(Pid, quit).
 
 %% ------------------------------------------------------------------
-%% gen_server Function Definitions
+%% gen_fsm Function Definitions
 %% ------------------------------------------------------------------
 
 init([]) ->
-    {ok, []}.
+    {ok, connected, []}.
 
-handle_call({nick, Nick}, _From, State) ->
+connected({nick, Nick}, _From, State) ->
     Reply = cerlicue_backend:nick(Nick, self()),
-    {reply, Reply, State};
-handle_call({privmsg, Name, Msg}, _From, State) ->
+    {reply, Reply, connected, State};
+connected({privmsg, Name, Msg}, _From, State) ->
     Reply = cerlicue_backend:privmsg(Name, Msg, self()),
-    {reply, Reply, State};
-handle_call({join, Channel}, _From, State) ->
+    {reply, Reply, connected, State};
+connected({join, Channel}, _From, State) ->
     Reply = cerlicue_backend:join(Channel, self()),
-    {reply, Reply, State};
-handle_call({topic, Channel}, _From, State) ->
+    {reply, Reply, connected, State};
+connected({topic, Channel}, _From, State) ->
     Reply = cerlicue_backend:topic(Channel),
-    {reply, Reply, State};
-handle_call({names, Channel}, _From, State) ->
+    {reply, Reply, connected, State};
+connected({names, Channel}, _From, State) ->
     Reply = cerlicue_backend:names(Channel),
-    {reply, Reply, State};
-handle_call({part, Channel, Msg}, _From, State) ->
+    {reply, Reply, connected, State};
+connected({part, Channel, Msg}, _From, State) ->
     Reply = cerlicue_backend:part(Channel, Msg, self()),
-    {reply, Reply, State}.
+    {reply, Reply, connected, State}.
 
-handle_cast(quit, State) ->
+connected(quit, State) ->
     {stop, normal, State}.
 
-handle_info({nick, Old, New}, State) ->
-    io:format("~p changed their nick to ~p~n", [Old, New]),
-    {noreply, State};
-handle_info({privmsg, From, To, Msg}, State) ->
-    io:format("~p --> ~p: ~p~n", [From, To, Msg]),
-    {noreply, State};
-handle_info({join, Nick, Channel}, State) ->
-    io:format("~p joined ~p~n", [Nick, Channel]),
-    {noreply, State};
-handle_info({part, Nick, Channel, Msg}, State) ->
-    io:format("~p departed ~p: ~p~n", [Nick, Channel, Msg]),
-    {noreply, State};
-handle_info({quit, Nick, Msg}, State) ->
-    io:format("~p quit: ~p~n", [Nick, Msg]),
-    {noreply, State}.
+handle_event(_Event, StateName, State) ->
+    {next_state, StateName, State}.
 
-terminate(_Reason, _State) ->
+handle_sync_event(_Event, _From, StateName, State) ->
+    {reply, ok, StateName, State}.
+
+handle_info({nick, Old, New}, StateName, State) ->
+    io:format("~p changed their nick to ~p~n", [Old, New]),
+    {next_state, StateName, State};
+handle_info({privmsg, From, To, Msg}, StateName, State) ->
+    io:format("~p --> ~p: ~p~n", [From, To, Msg]),
+    {next_state, StateName, State};
+handle_info({join, Nick, Channel}, StateName, State) ->
+    io:format("~p joined ~p~n", [Nick, Channel]),
+    {next_state, StateName, State};
+handle_info({part, Nick, Channel, Msg}, StateName, State) ->
+    io:format("~p departed ~p: ~p~n", [Nick, Channel, Msg]),
+    {next_state, StateName, State};
+handle_info({quit, Nick, Msg}, StateName, State) ->
+    io:format("~p quit: ~p~n", [Nick, Msg]),
+    {next_state, StateName, State}.
+
+terminate(_Reason, _StateName, _State) ->
     ok.
 
-code_change(_OldVsn, State, _Extra) ->
+code_change(_OldVsn, _StateName, State, _Extra) ->
     {ok, State}.
-
-%% ------------------------------------------------------------------
-%% Internal Function Definitions
-%% ------------------------------------------------------------------
